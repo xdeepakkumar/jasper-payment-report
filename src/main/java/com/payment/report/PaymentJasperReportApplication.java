f434b981cd83b293c5d9eb345c572875ed77ad26
dk <dk@gmail.com>
2022-07-10 00:37:09 +0530

    initial commit

---
+++ src/main/java/com/payment/report/PaymentJasperReportApplication.java
@@ -0,0 +1,13 @@
+package com.payment.report;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentJasperReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentJasperReportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/payment/report/PaymentJasperReportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payment.report;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentJasperReportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
